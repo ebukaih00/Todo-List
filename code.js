dd8dbd67e64425227061fea46037379f908e4678
Chukwuebuka ihueze <ihuezeebuka5@gmail.com>
2026-05-19 16:39:48 +0100

    feat: add introductory text to the todo list page

--- code.js
+++ code.js
@@ -4,38 +4,54 @@ const tasks = []
 let listEl = document.querySelector(".list-container")
 
 
+
+
 function addTask() {
 
     let input = document.getElementById("task-el")
     let taskEl = input.value
+    let errorEl = document.getElementById("error-el")
 
-    tasks.push({
-        text: taskEl
-    })
     
-    
-    listEl.innerHTML = ""
-    
-   
-    for (let i = 0; i < tasks.length; i++) {
-        listEl.innerHTML += `
-    
-    <div class ="list-item">
-    <p> ${tasks[i].text}</p>
-    <button id = "delete-btn" onclick = "deleteTask(${i})">Delete</button>
-    </div>
-    `
-    
+
+    if (input.value.trim() === "") {
+        errorEl.innerText = "Please enter a task! "
     }
-   
+
+    else {
+        errorEl.innerText = ""
+        listEl.innerHTML = ""
+        input.value = ""
+localStorage.setItem("task", JSON.stringify(tasks))
+        tasks.push({
+            text: taskEl
+        })
+
+
+
+
+
+
+        for (let i = 0; i < tasks.length; i++) {
+            listEl.innerHTML += `
+    
+    <div class ="list-item">
+    <p> ${tasks[i].text}</p>
+    <button id = "delete-btn" onclick = "deleteTask(${i})">Delete</button>
+    </div>
+    `
+
+        }
+    }
 
 }
 
 
 
-function deleteTask(index){
-     tasks.splice(index,1)
+function deleteTask(index) {
+    tasks.splice(index, 1)
     listEl.innerHTML = ""
+    localStorage.getItem(tasks)
 
     for (let i = 0; i < tasks.length; i++) {
         listEl.innerHTML += `
@@ -45,5 +61,5 @@ function deleteTask(index){
     <button id = "delete-btn" onclick = "deleteTask(${i})">Delete</button>
     </div>
     `
-}
+    }
 }
